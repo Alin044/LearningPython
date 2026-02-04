ca0975afdf729cc5a42ca5d98a6121c5901c2205
Alin044 <alinalin03030@gmail.com>
2026-02-04 05:34:20 +0000

    variables in python

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+#This is my first python comment
+print("Hello World")
 
+#string variables
+first_name = "Alin"
+print(first_name)
+print(f"Hello {first_name}")
+
+#Integers
+age = 21
+print(age)
+print(f"You are {age} years old")
+
+quantity = 3
+print(f"You are buying {quantity} items")
+
+#Boolean
+is_student = False
+if is_student:
+    print("You are a student")
+else :
+    print("You are not a student")
