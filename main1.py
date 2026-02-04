1f917afca560704194c1953a7a5c7a6abd1eae4a
Alin044 <alinalin03030@gmail.com>
2026-02-04 05:56:34 +0000

    input in python

---
+++ main1.py
@@ -0,0 +1,20 @@
+#Typecasting = the process of converting a variable from one data type to another
+#              str(), int(), float(), bool()
+
+name = "Alin"
+age = 21
+gpa = 3.1
+is_student = True
+
+print(type(name))  #find the type of the variable
+
+gpa = int(gpa)
+print(gpa)
+
+age = float(age)
+print(age)
+
+age = str(age)
+print(age)
+age += "1"
+print(age)
---
+++ main2.py
@@ -0,0 +1,9 @@
+#input() = A function that prompts the user to enter data
+#          returns the entered data as a string
+
+name = input("What is your name?")
+age = input("How old are you?")
+print(f"Hello {name}")
+print(f"You are {age} years old")
+
+
